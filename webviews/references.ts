7692a438e2a5d92f5e0ab79fec4271528df78d17
Kim Ying <kimying@ufl.edu>
2021-08-05 13:56:22 -0700

    Manually added string and operator references

--- webviews/references.ts
+++ webviews/references.ts
@@ -144,6 +144,7 @@ export class Ref {
 export let keywords = JSON.stringify(Ref.getAllKeywords());
 
 function initiateReferenceLibrary() {
+    // Scanner Refs
     new Ref( // 1
         "Scanner",
         ["scanner", "input", "read"],
@@ -191,6 +192,176 @@ function initiateReferenceLibrary() {
         "String stringName = scannerName.nextLine();", 
         ],
     );
+
+    // String methods
+
+    new Ref( // 6
+        "String",
+        ["string", "declare"],
+        "https://docs.oracle.com/javase/7/docs/api/java/lang/String.html",
+        [
+        "The String class represents character strings. All strings are instances of this class.",
+        "Declare a string like this:",
+        "String str = \"abc\";", 
+        ],
+    );
+
+    new Ref( // 7
+        "String: charAt()",
+        ["string", "character", "index"],
+        "https://www.tutorialspoint.com/java/java_strings.htm",
+        [
+        "Strings contain indexes of characters ranging from 0 to length minus 1.",
+        "The charAt() method can be used to return the character at a specific index of a string:",
+        "char char1 = stringName.charAt(index)", 
+        ],
+    );
+
+    new Ref( // 8
+        "String: length()",
+        ["string", "character", "length", "size", "count"],
+        "https://www.tutorialspoint.com/java/java_string_length.htm",
+        [
+        "Use length() to find the number of characaters in a string:",
+        "int stringLength = stringName.length();",
+        ],
+    );
+
+    new Ref( // 9
+        "String: equals()",
+        ["string", "equal", "equals", "same", "match"],
+        "https://www.w3schools.com/java/ref_string_equals.asp",
+        [
+        "Use the equals() method to compare two strings",
+        "If the strings are equal, it returns true, otherwise it returns false.",
+        "boolean equalStrings = str1.equals(str2);",
+        ],
+    );
+
+    new Ref( // 10
+        "String: startsWith()",
+        ["string", "starts", "begins"],
+        "https://www.w3schools.com/java/ref_string_startswith.asp",
+        [
+        "The startsWith() method checks whether a string starts with the specified character(s).",
+        "Example: String myStr1 = \"Hello\"",
+        "myStr1.startsWith(\"Hel\"); //true",
+        ],
+    );
+
+    new Ref( // 11
+        "String: substring()",
+        ["string", "substring"],
+        "https://www.w3schools.com/java/java_ref_string.asp",
+        [
+        "The subString() method returns a substring according to start and end indices. Example: ",
+        "String myStr1 = \"apple\"",
+        "int start = 1",
+        "int end = 4",
+        "String subStr = myStr1.subString(start, end);",
+        "//subStr = \"ppl\""
+        ],
+    );
+
+    new Ref( // 12
+        "String: compareTo()",
+        ["string", "compare"],
+        "https://www.w3schools.com/java/ref_string_compareto.asp",
+        [
+        "The compareTo() method compares two strings lexicographically.",
+        "The unicode values of each character in the strings are used to compare.",
+        "The method returns 0 if the strings are equal.",
+        "A negative int is returned if str1 < str2 (less characters)",
+        "A positive int is returned if str1 > str2 (more characters)",
+        "int i = str1.compareTo(str2)",
+        ],
+    );
+
+    // Operators
+
+    new Ref( // 13
+        "Relational Operators",
+        ["operators", "operator", "relation", "less", "greater", "equal", "equals"],
+        "https://docs.oracle.com/javase/tutorial/java/nutsandbolts/operators.html",
+        [
+        "equal to: ==",
+        "not equal to: !=",
+        "less than: <",
+        "greater than: >",
+        "less than or equal to: <=",
+        "greater than or equal to: >=",
+        ],
+    );
+
+    new Ref( // 14
+        "Logical Operators",
+        ["operator", "symbol", "logic", "operators"],
+        "https://docs.oracle.com/javase/tutorial/java/nutsandbolts/operators.html",
+        [
+        "logical AND: &&",
+        "logical OR: ||",
+        "logical NOT: !"
+        ],
+    );
+
+    new Ref( // 14
+        "Operators: Equals",
+        ["operator", "symbol", "operators", "equal", "equals", "check"],
+        "https://www.tutorialspoint.com/java/java_basic_operators.htm",
+        [
+        "The equality operator is represented by '=='.",
+        "The statement will return true if two variables are equal.",
+        "Note: do not use this to check string equality. Use .equals() instead.",
+        "ex: if (var1 == var2) { // do something } ",
+        ],
+    );
+
+    new Ref( // 15
+        "Operators: Less Than",
+        ["operator", "operators", "less", "smaller", "check", "compare"],
+        "https://www.tutorialspoint.com/java/java_basic_operators.htm",
+        [
+        "The '<' operator checks if the value on the left is less than the value on the right.",
+        "If the value on the left is less, than it returns true.",
+        "  if (var1 < var2) { // var1 is less}"
+        ],
+    );
+
+    new Ref( // 16
+        "Operators: Greater Than",
+        ["operator", "operators", "greater", "larger", "bigger", "check", "compare"],
+        "https://www.tutorialspoint.com/java/java_basic_operators.htm",
+        [
+        "The '>' operator checks if the value on the left is greater than the right.",
+        "If the value on the left is greater, than it returns true.",
+        "  if (var1 > var2) { // var1 is bigger}"
+        ],
+    );
+
+    new Ref( // 17
+        "Operators: AND",
+        ["operator", "operators", "logical", "check", "both", "logic"],
+        "https://www.geeksforgeeks.org/java-logical-operators-with-examples/",
+        [
+        "The logical AND operator is signified with '&&'.",
+        "It will return true only when both conditions are satisfied.", 
+        "  if (condition1 && condition2) // both conditions are true"
+        ],
+    );
+
+    new Ref( // 18
+        "Operators: OR",
+        ["operator", "logical", "operators", "logic", "check"],
+        "https://www.geeksforgeeks.org/java-logical-operators-with-examples/",
+        [
+        "The logical OR operator is signified with '||'.",
+        "The operator will return true if at least one of the conditions is true.",
+        "ex: if (condition1 || condition2) // one or both are true ",
+        ],
+    );
+
+    // Exceptions/Errors
+
 
     new Ref(
         "ASCII Table",
